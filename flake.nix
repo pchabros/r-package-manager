64b9075e4ed5dc797e1519a70ff7ce4b883d76bf
pchabros <pawel.chabros@yahoo.pl>
2025-05-20 22:06:34 +0200

    R app flake template added

--- flake.nix
+++ flake.nix
@@ -4,37 +4,35 @@
     flake-parts.url = "github:hercules-ci/flake-parts";
     haskell-flake.url = "github:srid/haskell-flake";
   };
-  outputs =
-    inputs@{ nixpkgs, flake-parts, ... }:
-
-    flake-parts.lib.mkFlake { inherit inputs; } {
-
+  outputs = inputs @ {
+    nixpkgs,
+    flake-parts,
+    ...
+  }:
+    flake-parts.lib.mkFlake {inherit inputs;} {
       systems = nixpkgs.lib.systems.flakeExposed;
-      imports = [ inputs.haskell-flake.flakeModule ];
-
-      perSystem =
-        {
-          self',
-          pkgs,
-          config,
-          ...
-        }:
-        {
-          haskellProjects.default = {
-            packages = { };
-            settings = { };
-            devShell = { };
-            autoWire = [
-              "packages"
-              "apps"
-              "checks"
-            ];
-          };
-
-          devShells.default = pkgs.mkShell {
-            inputsFrom = [ config.haskellProjects.default.outputs.devShell ];
-            packages = with pkgs; [ just ];
+      imports = [inputs.haskell-flake.flakeModule];
+      perSystem = {
+        self',
+        pkgs,
+        config,
+        ...
+      }: {
+        haskellProjects.default = {
+          autoWire = ["apps" "checks"];
+        };
+        devShells.default = pkgs.mkShell {
+          inputsFrom = [config.haskellProjects.default.outputs.devShell];
+          packages = with pkgs; [just];
+        };
+      };
+      flake = {
+        templates = {
+          app = {
+            path = ./templates/app;
+            description = "R Package Manager application";
           };
         };
+      };
     };
 }
