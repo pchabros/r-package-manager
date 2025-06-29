6d4baa23f2bfd7828236e96cec9d43c3df84c896
pchabros <pawel.chabros@yahoo.pl>
2025-06-29 20:57:53 +0200

    WIP: Add specific package version

--- src/Cmd.hs
+++ src/Cmd.hs
@@ -1,31 +1,63 @@
 module Cmd where
 
-import Options.Applicative (
-  CommandFields,
-  Mod,
-  Parser,
-  ParserInfo,
-  argument,
-  command,
-  fullDesc,
-  header,
-  helper,
-  info,
-  metavar,
-  progDesc,
-  str,
-  subparser,
- )
+import Options.Applicative
+  ( CommandFields,
+    Mod,
+    Parser,
+    ParserInfo,
+    argument,
+    command,
+    fullDesc,
+    header,
+    helper,
+    info,
+    maybeReader,
+    metavar,
+    progDesc,
+    subparser,
+  )
+import Text.Megaparsec (Parsec, parseMaybe)
+import Text.Megaparsec.Char (alphaNumChar, char, numberChar)
+import Text.Read (read)
 
 newtype Opts = Opts {cmd :: Cmd}
 
-data Cmd = Init | Add [Text] | Remove [Text]
+data Cmd = Init | Add [Package] | Remove [Package]
+
+data Package = Package {name :: Text, version :: Version} deriving stock (Show)
+
+data Version = Version
+  { major :: Int,
+    minor :: Int,
+    patch :: Int
+  }
+  deriving stock (Show)
 
 initCmd :: Mod CommandFields Cmd
 initCmd = command "init" (info (pure Init) (progDesc "Init r-package-manager project"))
 
-packagesParser :: Parser [Text]
-packagesParser = some (argument str (metavar "PACKAGES"))
+packagesParser :: Parser [Package]
+packagesParser = some (argument pkg (metavar "PACKAGES"))
+  where
+    pkg = maybeReader $ parseMaybe pPackage
+
+type MParser = Parsec Void String
+
+pNumber :: MParser Int
+pNumber = read <$> many numberChar
+
+pVersion :: MParser Version
+pVersion =
+  Version
+    <$> pNumber
+    <*> (char '.' *> pNumber)
+    <*> (char '.' *> pNumber)
+
+pPackage :: MParser Package
+pPackage =
+  Package
+    <$> (toText <$> many (alphaNumChar <|> char '.'))
+    <*> (char '@' *> pVersion)
 
 addOpts :: Parser Cmd
 addOpts = Add <$> packagesParser
@@ -41,5 +73,5 @@ removeCmd = command "remove" (info removeOpts (progDesc "Remove R packages"))
 
 optsParser :: ParserInfo Opts
 optsParser = info (parser <**> helper) (fullDesc <> header "R Package Manager")
- where
-  parser = Opts <$> subparser (initCmd <> addCmd <> removeCmd)
+  where
+    parser = Opts <$> subparser (initCmd <> addCmd <> removeCmd)
--- src/Handle.hs
+++ src/Handle.hs
@@ -1,11 +1,32 @@
 module Handle where
 
-import Data.Text (intercalate)
+import Cmd (Package)
 import Manifest qualified
 import System.Directory (getCurrentDirectory)
 import System.FilePath (takeBaseName)
-import System.Process (callCommand)
+import System.Process (callCommand, createProcess, proc)
 import Prelude hiding (intercalate)
+
+-- -- nix hash convert --hash-algo sha256 $(nix-prefetch-url 'https://cran.r-project.org/src/contrib/Archive/dplyr/dplyr_1.1.3.tar.gz')
+-- processStdout :: Text -> [Text] -> IO Text
+-- processStdout cmd args = do
+--   (_, Just hout, _, _) <- createProcess (proc (toString cmd) (map toString args))
+--   return $ show hout
+--
+-- packageUrl :: Text -> Text -> Text -- TODO: create `data Package`
+-- packageUrl package version =
+--   "https://cran.r-project.org/src/contrib/Archive/"
+--     <> package
+--     <> "/"
+--     <> package
+--     <> "_"
+--     <> version
+--     <> ".tar.gz"
+--
+-- prefetchPackage :: Text -> Text -> IO Text
+-- prefetchPackage package version = processStdout "nix-prefetch-url" [packageUrl package version]
+--
+-- --
 
 init :: IO ()
 init = do
@@ -18,20 +39,24 @@ init = do
   callCommand "nix flake init --template github:pchabros/r-package-manager#app"
   callCommand "direnv allow"
   file <- Manifest.read
-  Manifest.save file{Manifest.name}
+  Manifest.save file {Manifest.name}
 
-add :: [Text] -> IO ()
+add :: [Package] -> IO ()
 add packages = do
-  file <- Manifest.read
-  Manifest.save
-    file{Manifest.packages = ordNub $ sort $ file.packages ++ packages}
-  putTextLn $ "Adding packages: " <> intercalate ", " packages
-  callCommand "direnv reload"
+  putStrLn "########################"
+  print packages
+  putStrLn "########################"
 
-remove :: [Text] -> IO ()
+-- file <- Manifest.read
+-- Manifest.save
+--   file {Manifest.packages = ordNub $ sort $ file.packages ++ packages}
+-- putTextLn $ "Adding packages: " <> intercalate ", " packages
+-- callCommand "direnv reload"
+
+remove :: [Package] -> IO ()
 remove packages = do
   file <- Manifest.read
   Manifest.save
-    file{Manifest.packages = filter (`notElem` packages) file.packages}
-  putTextLn $ "Removing packages: " <> intercalate ", " packages
+    file -- {Manifest.packages = filter (`notElem` packages) file.packages}
+    -- putTextLn $ "Removing packages: " <> intercalate ", " packages
   callCommand "direnv reload"
